16114bb23dc6ccf30aea19afcb211f51a54aa991
Vignesh G <vignesh1998.vg@gmail.com>
2019-03-27 10:13:28 +0530

    Update ClerkController.java

--- src/com/pta/controller/ClerkController.java
+++ src/com/pta/controller/ClerkController.java
@@ -1,5 +1,7 @@
 package com.pta.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -8,48 +10,48 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-import com.pta.model.ClerkPOJO;
+import com.pta.model.ClerkPojo;
 import com.pta.service.ClerkService;
 import com.pta.service.ClerkServiceImpl;
 
 @Controller
 public class ClerkController {
 	
-	@RequestMapping("/listAllClerk")
-	public String listAllClerk() {
-		return "ListAllClerk";
+	StringBuilder builder = new StringBuilder();
+	int check = 0; 
+	
+	@RequestMapping(value = "/listAllClerk") 
+	public String listAllClerk(HttpServletRequest request,ModelMap map) {
+		ClerkService clerkService=new ClerkServiceImpl();
+		ArrayList clerkDetails = clerkService.fetchClerkDetails();
+		if(check == 1)
+		{
+			String id = builder.toString();
+			map.addAttribute("id", id);
+			builder.setLength(0);
+			request.setAttribute("success", "success");
+		}
+		request.setAttribute("clerkDetails", clerkDetails);
+	    return "ListAllClerk"; 
 	}
 	
-	@RequestMapping("/addClerkForm")
-	public String addClerkForm(ModelMap map) {
-		map.addAttribute("clerk", new ClerkPOJO());
-		return "AddClerk";
+	@RequestMapping("/enterClerkDetails")
+	public String enterClerkDetails(ModelMap model) {
+		model.addAttribute("clerk", new ClerkPojo()); 
+		return "EnterClerkDetails";
 	}
-	
 	
 	@RequestMapping(value="/addClerk", method=RequestMethod.POST)
-	public String addClerk(HttpServletRequest request, ModelMap map, @ModelAttribute("admin") ClerkPOJO pojo) {
+	public String addClerk(HttpServletRequest request, @ModelAttribute("clerk") ClerkPojo pojo) {
 		
 		ClerkService clerkService=new ClerkServiceImpl();
-
-		pojo.setFirstName(request.getParameter("fname"));
-		pojo.setLastName(request.getParameter("lname"));
-		pojo.setAge(Integer.parseInt(request.getParameter("age")));
-		pojo.setAddressLine1(request.getParameter("addressl1"));
-		pojo.setAddressLine2(request.getParameter("addressl2"));
-		pojo.setAlternateContactNumber(Integer.parseInt(request.getParameter("altcontact")));
-		pojo.setCity(request.getParameter("city"));
-		pojo.setContactNumber(Integer.parseInt(request.getParameter("contactnumber")));
-		pojo.setDateOfBirth(request.getParameter("dob"));
-		pojo.setEmailId(request.getParameter("email"));
-		pojo.setGender(request.getParameter("gender"));
-		pojo.setState(request.getParameter("state"));
-		pojo.setZipCode(request.getParameter("zipcode"));
 		
-
-		clerkService.addClerkDetails(pojo);
-		return "ListAllClerk";
+		
+		String id = clerkService.addClerkDetails(pojo);
+		builder.append(id);
+		
+		check = 1; 
+		return "redirect:listAllClerk";
 	}
 
 }
