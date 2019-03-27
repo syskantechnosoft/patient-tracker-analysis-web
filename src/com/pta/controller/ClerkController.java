ac41d3fdacd117f1edc7ee5cf97d3010cb919de7
Pratik Pritam <pratikpritam347@gmail.com>
2019-03-27 12:29:16 +0530

    Add files via upload

--- src/com/pta/controller/ClerkController.java
+++ src/com/pta/controller/ClerkController.java
@@ -10,48 +10,42 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.pta.model.ClerkPojo;
+import com.pta.model.ClerkPOJO;
 import com.pta.service.ClerkService;
 import com.pta.service.ClerkServiceImpl;
 
 @Controller
 public class ClerkController {
 	
-	StringBuilder builder = new StringBuilder();
-	int check = 0; 
-	
-	@RequestMapping(value = "/listAllClerk") 
-	public String listAllClerk(HttpServletRequest request,ModelMap map) {
-		ClerkService clerkService=new ClerkServiceImpl();
+	@RequestMapping(value="/listAllClerk")
+	public String listAllClerk(HttpServletRequest request) {
+		
+		ClerkService clerkService = new ClerkServiceImpl();
 		ArrayList clerkDetails = clerkService.fetchClerkDetails();
-		if(check == 1)
-		{
-			String id = builder.toString();
-			map.addAttribute("id", id);
-			builder.setLength(0);
-			request.setAttribute("success", "success");
-		}
 		request.setAttribute("clerkDetails", clerkDetails);
-	    return "ListAllClerk"; 
+		return "ListAllClerk";
 	}
 	
-	@RequestMapping("/enterClerkDetails")
-	public String enterClerkDetails(ModelMap model) {
-		model.addAttribute("clerk", new ClerkPojo()); 
-		return "EnterClerkDetails";
+	
+	@RequestMapping("/clerkAddition")
+	public String clerkAddition(ModelMap map) {
+		map.addAttribute("clerk", new ClerkPOJO());
+		return "AddClerk";
 	}
+	
 	
 	@RequestMapping(value="/addClerk", method=RequestMethod.POST)
-	public String addClerk(HttpServletRequest request, @ModelAttribute("clerk") ClerkPojo pojo) {
+	public String addClerk(HttpServletRequest request, ModelMap map, @ModelAttribute("clerk") ClerkPOJO pojo) {
 		
-		ClerkService clerkService=new ClerkServiceImpl();
-		
-		
+		ClerkService clerkService=new ClerkServiceImpl();		
 		String id = clerkService.addClerkDetails(pojo);
-		builder.append(id);
+		map.addAttribute("id", id);
+		request.setAttribute("success", "successs");
 		
-		check = 1; 
-		return "redirect:listAllClerk";
+		ArrayList clerkDetails = clerkService.fetchClerkDetails();
+		request.setAttribute("clerkDetails", clerkDetails);
+		
+		return "ListAllClerk";
 	}
 
 }
